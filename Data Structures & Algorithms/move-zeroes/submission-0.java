7e3e0133ba212b0933dbe370324eb9b9bf14165f
Nuraly <94793995+nuraly04@users.noreply.github.com>
2026-05-15 11:44:58 +0600

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int right = 0;
+        int left = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+
+        while (left < nums.length) {
+            nums[left] = 0;
+            left++;
+        }
+    }
+}
