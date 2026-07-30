423bc0e23ffa3cbb0d2843fd11066d955a385cf0
Nuraly <94793995+nuraly04@users.noreply.github.com>
2026-07-30 13:07:12 +0600

    Update: two-integer-sum - submission-8

--- Data Structures & Algorithms/two-integer-sum/submission-8.java
+++ Data Structures & Algorithms/two-integer-sum/submission-8.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                return new int[] {map.get(nums[i]), i};
+                    return new int[] {map.get(nums[i]), i};
             }
-
             map.put(target - nums[i], i);
         }
-        return new int[] {};
+
+        return new int[]{};
     }
 }
