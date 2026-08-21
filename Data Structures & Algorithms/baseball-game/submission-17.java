5412f28128157fde0a39f7bd726ec333a63ac9c6
Nuraly <94793995+nuraly04@users.noreply.github.com>
2026-08-22 01:49:12 +0600

    Add: baseball-game - submission-17

---
+++ Data Structures & Algorithms/baseball-game/submission-17.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+
+        for (String operation : operations) {
+            if (operation.equals("+")) {
+                
+                int y = stack.peek() + stack.get(stack.size() - 2);
+                stack.push(y);
+                sum+=y;
+                continue;
+            } else if (operation.equals("D")) {
+                int y = stack.peek() * 2;
+                stack.push(y);
+                sum+=y;
+                continue;
+            } else if (operation.equals("C")) {
+                sum -= stack.pop();
+                continue;
+            }
+
+            int y = Integer.parseInt(operation);
+            stack.push(y);
+            sum+=y;
+        }
+
+        return sum;
+    }
+}
